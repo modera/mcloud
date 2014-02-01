636e6d5f22d658a5fa20e9a29501707ff8c8e43c
Alex Rudakov <ribozz@gmail.com>
2014-02-01 21:47:36 +0200

    Initial project structure

---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+figaro
+======
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+.. only:: html
+
+   Indices and tables
+   ==================
+
+   * :ref:`genindex`
+   * :ref:`modindex`
+   * :ref:`search`
---
+++ figaro/metadata.py
@@ -0,0 +1,17 @@
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'figaro'
+project = "figaro"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'Production cloud deployments of fig infrastructure with docker'
+authors = ['Alex Rudakov']
+authors_string = ', '.join(authors)
+emails = ['ribozz@gmail.com']
+license = 'Apache License'
+copyright = '2013 ' + authors_string
+url = 'http://pywizard.com/figaro/'
