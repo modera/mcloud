549aa55c22e784ae213df62861420fdbe02d85da
Alex Rudakov <ribozz@gmail.com>
2014-10-14 09:03:57 +0300

    Fix path to version.txt

--- mcloud/metadata.py
+++ mcloud/metadata.py
@@ -2,12 +2,15 @@
 
 Information describing the project.
 """
+import os
+
+root_dir = os.path.dirname(os.path.dirname(__file__))
 
 # The package name, which is also the "UNIX name" for the project.
 package = 'mcloud'
 project = "mcloud"
 project_no_spaces = project.replace(' ', '')
-version = open('version.txt').read()
+version = open(root_dir + '/version.txt').read()
 description = 'Production cloud deployments of fig infrastructure with docker'
 authors = ['Alex Rudakov']
 authors_string = ', '.join(authors)
