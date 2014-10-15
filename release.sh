cc93ade07e6b771192db06b2cfe820c813f4d44e
Alex Rudakov <ribozz@gmail.com>
2014-10-15 15:07:46 +0300

    New version

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+pip install -e .
+pip install -r requirements-dev.txt
+py.test tests
+
+VERSION=`python release.py`
+
+if [ $VERSION = "0" ]; then
+    echo "Version already deployed."
+    exit 0;
+fi
+
+echo "version = '$VERSION'" > mcloud/version.py
+
+dpkg-buildpackage -us -uc
+
+python setup.py sdist register upload
+
+git push --tags
