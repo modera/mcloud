e6448d13db98a2db94a5afcfbaf1047d07c529ef
Alex <ribozz@gmail.com>
2014-10-20 23:42:16 +0300

    Start implementing Ctrl + c proper handling

--- mcloud/rpc_client.py
+++ mcloud/rpc_client.py
@@ -6,8 +6,8 @@ import argparse
 import readline
 import subprocess
 from bashutils.colors import color_text
+import signal
 from mcloud.attach import Terminal, AttachStdinProtocol
-from mcloud.shell import BufferAwareCompleter
 from mcloud.sync.client import FileServerError
 from mcloud.sync.storage import get_storage, storage_sync
 from mcloud.util import txtimeout
@@ -722,119 +722,8 @@ def main(argv):
     # client = ApiRpcClient(host=args.host, settings=settings)
 
     if len(argv) < 2:
-        # Register our completer function
-        readline.set_completer(BufferAwareCompleter(
-            {'list': ['files', 'directories'],
-             'print': ['byname', 'bysize'],
-             'stop': [],
-            }).complete)
-
         # Use the tab key for completion
-        readline.parse_and_bind('tab: complete')
 
-        state = {
-            'app': None,
-            'host': 'me',
-        }
-
-
-        def green(text):
-            print(color_text(text, color='green'))
-
-        def yellow(text):
-            print(color_text(text, color='blue', bcolor='yellow'))
-
-        def info(text):
-            print()
-
-        @cli('Set current application', by_ref=True, arguments=(
-            arg('name', help='Application name', default=None, nargs='?'),
-        ))
-        def use(name, **kwargs):
-            if '@' in name:
-                app, host = name.split('@')
-                if host.strip() == '':
-                    host = 'me'
-                if app.strip() == '':
-                    app = None
-
-                state['app'] = app
-                state['host'] = host
-            else:
-                state['app'] = name
-
-
-        @inlineCallbacks
-        def mcloud_shell():
-
-            from mcloud.logo import logo
-            print(logo)
-
-            histfile = os.path.join(os.path.expanduser("~"), ".mcloud_history")
-            try:
-                readline.read_history_file(histfile)
-            except IOError:
-                pass
-
-            line = ''
-            while line != 'exit':
-                print('')
-                prompt = 'mcloud: %s@%s> ' % (state['app'] or '~', state['host'])
-
-                try:
-                    line = None
-                    line = raw_input(color_text(prompt, color='white', bcolor='blue') + ' ')
-
-                    if line == '':
-                        continue
-
-                    if line == 'exit':
-                        break
-                    params = line.split(' ')
-                    args = arg_parser.parse_args(params)
-
-                    args.argv0 = argv[0]
-
-                    if args.host:
-                        host = args.host
-                    elif state['host'] == 'me' or not state['host']:
-                        host = '127.0.0.1'
-                    else:
-                        host = state['host']
-                    client = ApiRpcClient(host=host, settings=settings)
-
-                    for key, val in state.items():
-                        if not hasattr(args, key) or not getattr(args, key):
-                            setattr(args, key, val)
-
-                    if isinstance(args.func, str):
-                        yield getattr(client, args.func)(**vars(args))
-                    else:
-                        yield args.func(**vars(args))
-
-                except SystemExit:
-                    pass
-                except KeyboardInterrupt:
-                    continue
-                except EOFError:
-                    print('')
-                    break
-
-                except Exception as e:
-                    print color_text('Error:', color='white', bcolor='red')
-                    print(e)
-
-            readline.write_history_file(histfile)
-            reactor.callFromThread(reactor.stop)
-
-
-        # def heartbeat():
-        #     print "heartbeat"
-        #     reactor.callLater(1.0, heartbeat)
-        #
-        # reactor.callLater(1.0, heartbeat)
-
-        mcloud_shell()
 
         reactor.run()
     else:
--- mcloud/shell.py
+++ mcloud/shell.py
@@ -1,67 +1,133 @@
-import readline
+import json
 import logging
+import sys
+import uuid
+import argparse
+import readline
+import subprocess
+from bashutils.colors import color_text
+import signal
+from mcloud.attach import Terminal, AttachStdinProtocol
+from mcloud.sync.client import FileServerError
+from mcloud.sync.storage import get_storage, storage_sync
+from mcloud.util import txtimeout
 
-LOG_FILENAME = '/tmp/completer.log'
-logging.basicConfig(filename=LOG_FILENAME,
-                    level=logging.DEBUG,
-                    )
+import re
+import os
+from autobahn.twisted.util import sleep
+from confire import Configuration
+import pprintpp
+from prettytable import PrettyTable, ALL
+from texttable import Texttable
+from twisted.internet import reactor, defer, stdio
+from twisted.internet.defer import inlineCallbacks
+from twisted.internet.error import TimeoutError
+from twisted.internet.error import ConnectionRefusedError
+from twisted.python import log
+from mcloud import metadata
 
-class BufferAwareCompleter(object):
 
-    def __init__(self, options):
-        self.options = options
-        self.current_candidates = []
-        return
 
-    def complete(self, text, state):
-        response = None
-        if state == 0:
-            # This is the first time for this text, so build a match list.
+def green(text):
+    print(color_text(text, color='green'))
 
-            origline = readline.get_line_buffer()
-            begin = readline.get_begidx()
-            end = readline.get_endidx()
-            being_completed = origline[begin:end]
-            words = origline.split()
+def yellow(text):
+    print(color_text(text, color='blue', bcolor='yellow'))
 
-            logging.debug('origline=%s', repr(origline))
-            logging.debug('begin=%s', begin)
-            logging.debug('end=%s', end)
-            logging.debug('being_completed=%s', being_completed)
-            logging.debug('words=%s', words)
+def info(text):
+    print()
 
-            if not words:
-                self.current_candidates = sorted(self.options.keys())
-            else:
-                try:
-                    if begin == 0:
-                        # first word
-                        candidates = self.options.keys()
-                    else:
-                        # later word
-                        first = words[0]
-                        candidates = self.options[first]
+@inlineCallbacks
+def mcloud_shell(subparsers, arg_parser):
+    readline.parse_and_bind('tab: complete')
 
-                    if being_completed:
-                        # match options with portion of input
-                        # being completed
-                        self.current_candidates = [ w for w in candidates
-                                                    if w.startswith(being_completed) ]
-                    else:
-                        # matching empty string so use all candidates
-                        self.current_candidates = candidates
+    state = {
+        'app': None,
+        'host': 'me',
+    }
 
-                    logging.debug('candidates=%s', self.current_candidates)
+    def use(name, **kwargs):
+        if '@' in name:
+            app, host = name.split('@')
+            if host.strip() == '':
+                host = 'me'
+            if app.strip() == '':
+                app = None
 
-                except (KeyError, IndexError), err:
-                    logging.error('completion error: %s', err)
-                    self.current_candidates = []
+            state['app'] = app
+            state['host'] = host
+        else:
+            state['app'] = name
 
+    cmd = subparsers.add_parser('use')
+    cmd.add_argument('name', help='Application name', default=None, nargs='?')
+
+
+        from mcloud.logo import logo
+        print(logo)
+
+        histfile = os.path.join(os.path.expanduser("~"), ".mcloud_history")
         try:
-            response = self.current_candidates[state]
-        except IndexError:
-            response = None
-        logging.debug('complete(%s, %s) => %s', repr(text), state, response)
-        return response
+            readline.read_history_file(histfile)
+        except IOError:
+            pass
+
+        line = ''
+        while line != 'exit':
+            print('')
+            prompt = 'mcloud: %s@%s> ' % (state['app'] or '~', state['host'])
+
+            try:
+                line = None
+                line = raw_input(color_text(prompt, color='white', bcolor='blue') + ' ')
+
+                if line == '':
+                    continue
+
+                if line == 'exit':
+                    break
+
+                readline.write_history_file(histfile)
+
+                params = line.split(' ')
+                args = arg_parser.parse_args(params)
+
+                args.argv0 = sys.argv[0]
+
+                if args.host:
+                    host = args.host
+                elif state['host'] == 'me' or not state['host']:
+                    host = '127.0.0.1'
+                else:
+                    host = state['host']
+                client = ApiRpcClient(host=host, settings=settings)
+
+                for key, val in state.items():
+                    if not hasattr(args, key) or not getattr(args, key):
+                        setattr(args, key, val)
+
+                if isinstance(args.func, str):
+                    yield getattr(client, args.func)(**vars(args))
+                else:
+                    yield args.func(**vars(args))
+
+            except SystemExit:
+                pass
+            except KeyboardInterrupt:
+                continue
+            except EOFError:
+                print('')
+                break
+
+            except Exception as e:
+                print color_text('Error:', color='white', bcolor='red')
+                print(e)
+
+        reactor.callFromThread(reactor.stop)
 
 
+    # def heartbeat():
+    #     print "heartbeat"
+    #     reactor.callLater(1.0, heartbeat)
+    #
+    # reactor.callLater(1.0, heartbeat)
