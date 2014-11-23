ef1c7ee5255dfa6175a33ec0ad5572e9cfcdc394
Alex <ribozz@gmail.com>
2014-11-23 18:50:33 +0200

    Logs using terminal implemented

--- mcloud/static/assets/js/app.js
+++ mcloud/static/assets/js/app.js
@@ -2,13 +2,13 @@
 var mcloudApp = angular.module('mcloudApp', []);
 
 
-
 mcloudApp.controller('McloudAppsCtrl', function ($scope, $interval) {
     var io = new McloudIO(location.hostname);
 
     $scope.selection = {
         app: 'new',
-        service: null
+        service: null,
+        service_tab: null
     };
 
     $scope.service_tasks = [];
@@ -31,24 +31,51 @@ mcloudApp.controller('McloudAppsCtrl', function ($scope, $interval) {
     $scope.select_service = function(service) {
         $scope.kill_service_tasks();
 
+        if ($scope.selection.service && $scope.selection.service.terminal) {
+            $scope.selection.service.terminal.destroy();
+        }
+
         $scope.selection.service = service;
+
+        if ($scope.selection.service_tab == 'inspect' || ! $scope.selection.service_tab) {
+            $scope.service_inspect(service);
+        }
+
+        if ($scope.selection.service_tab == 'logs') {
+            $scope.service_logs(service);
+        }
+    };
+
+    $scope.service_inspect = function(service) {
+        $scope.selection.service_tab = 'inspect';
 
         io.inspect($scope.selection.app.name, $scope.selection.service.shortname).then(function(result) {
             service.inspect = result;
             $scope.$apply();
         });
-
-        $scope.service_logs(service);
     };
 
     $scope.service_logs = function(service) {
-        io.logs(service.name).then(function(task) {
+        $scope.selection.service_tab = 'logs';
 
+        if (!service.terminal) {
+            service.terminal = new Terminal({
+              cols: 80,
+              rows: 24,
+              screenKeys: true
+            });
+        }
+
+        io.logs(service.name).then(function(task) {
             $scope.service_tasks.push(task.id);
 
             task.on('progress', function(data) {
-//                console.log(data);
-            })
+                if (service.terminal) {
+                    service.terminal.writeln(data);
+                }
+            });
+
+            $scope.$apply();
         });
     };
 
@@ -70,4 +97,24 @@ mcloudApp.controller('McloudAppsCtrl', function ($scope, $interval) {
 
     $scope.update_apps();
 
+});
+
+mcloudApp.directive('xterm', function() {
+
+  function link(scope, element, attrs) {
+      var term = null;
+
+      scope.$watch(attrs.term, function (value){
+          term = value;
+
+          if (term) {
+              term.open(element[0]);
+          }
+
+      });
+  }
+
+  return {
+      link: link
+  };
 });
