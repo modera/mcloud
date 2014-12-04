db8c0ffa894b4660cfef89e15d8492b1fc1b077f
Alex <ribozz@gmail.com>
2014-12-05 01:25:32 +0200

    SSL for web

--- mcloud/ssl.py
+++ mcloud/ssl.py
@@ -1,6 +1,7 @@
 import inject
 
 from twisted.internet import ssl
+from twisted.internet.iocpreactor import reactor
 
 
 class CtxFactory(ssl.ClientContextFactory):
@@ -18,3 +19,38 @@ class CtxFactory(ssl.ClientContextFactory):
         ctx.use_privatekey_file(self.key)
 
         return ctx
+
+
+def verifyCallback(connection, x509, errnum, errdepth, ok):
+
+    if not ok:
+        print 'invalid cert from subject:', x509.get_subject()
+        return False
+    else:
+        print 'Subject is: %s' % x509.get_subject().commonName
+        print "Certs are fine"
+        # return False
+    return True
+
+def listen_ssl(resource, interface, port):
+
+    settings = inject.instance('settings')
+
+    from OpenSSL import SSL
+
+    from twisted.internet import ssl
+
+    myContextFactory = ssl.DefaultOpenSSLContextFactory(
+        settings.ssl.key, settings.ssl.cert
+    )
+    ctx = myContextFactory.getContext()
+
+    ctx.set_verify(SSL.VERIFY_PEER | SSL.VERIFY_FAIL_IF_NO_PEER_CERT, verifyCallback)
+
+    # Since we have self-signed certs we have to explicitly
+    # tell the server to trust them.
+
+    ctx.load_verify_locations(settings.ssl.ca)
+    ctx.set_session_id('id')
+
+    reactor.listenSSL(port, resource, myContextFactory, interface=interface)
--- mcloud/web.py
+++ mcloud/web.py
@@ -1,13 +1,51 @@
+from mcloud.ssl import listen_ssl
 import os
 from klein import Klein
+from twisted.internet import reactor
+from twisted.web.server import Site
 from twisted.web.static import File
 from pkg_resources import resource_filename
 
+static_dir = resource_filename(__name__, 'static/')
+
 app = Klein()
+@app.route('/', branch=True)
+def static(request):
+    return File(static_dir)
+
+app_redirect = Klein()
+@app_redirect.route('/', branch=True)
+def static(request):
+    request.redirect('https://%s' % request.)
+    return File(static_dir)
+
+
+app_redirect = Klein()
 static_dir = resource_filename(__name__, 'static/')
 
 @app.route('/', branch=True)
 def static(request):
     return File(static_dir)
 
-mcloud_web = app.resource
+mcloud_web_redirect = app.resource
+
+
+
+def listen_web(settings):
+    """
+    @type settings: mcloud.rpc_server.McloudConfiguration
+    """
+
+    if settings and settings.ssl.enabled:
+
+        print '*' * 40
+        print 'Listen web on port *:7085 (SSL)'
+        print '*' * 40
+
+        listen_ssl(Site(mcloud_web()), settings.web_ip, 443)
+    else:
+        print '*' * 40
+        print 'Listen web on port *:7085 (Insecure)'
+        print '*' * 40
+
+        reactor.listenTCP(7085, Site(mcloud_web()), interface='127.0.0.1')
